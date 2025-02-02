8ac1f5af853ff2938e00e2941857001663f0d0c1
joker-ksh <amanmaurya3721@gmail.com>
2025-02-02 23:41:38 +0530

    sdfsags

--- src/Components/BasicTable.jsx
+++ src/Components/BasicTable.jsx
@@ -10,6 +10,7 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setData } from '../Redux/dataslice';
+import { Grid } from '@mui/material';
 
 function createData(userId, id, title, completed) {
   return { userId, id, title, completed };
@@ -19,10 +20,6 @@ export default function BasicTable() {
   const dispatch = useDispatch();
   const rows = useSelector((state) => state.data.value);
 
-
-  //this is useEffect Hook used to fetch the data from the Api and store the response in the Redux store
-  //Usually in React lifecycle methods the useEffect Hook is used to perform side effects(After browser coloring) in functional components
-  //As we know that this is some how similar to async await in javascript which keeps a promise and waits for the response(asyncronus programming)
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -39,32 +36,33 @@ export default function BasicTable() {
   }, [dispatch]);
 
   return (
-
-    <div className="w-full lg:w-1/2"> {/* this will take care for taking half width on desktop and  */}
-      <TableContainer component={Paper} className="overflow-x-auto">
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell className="font-bold">User ID</TableCell>
-              <TableCell align="right" className="font-bold">ID</TableCell>
-              <TableCell align="right" className="font-bold">Title</TableCell>
-              <TableCell align="right" className="font-bold">Completed</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.id} className="hover:bg-gray-100">
-                <TableCell component="th" scope="row">
-                  {row.userId}
-                </TableCell>
-                <TableCell align="right">{row.id}</TableCell>
-                <TableCell align="right">{row.title}</TableCell>
-                <TableCell align="right">{row.completed ? 'Yes' : 'No'}</TableCell>
+    <Grid container>
+      <Grid item xs={12} md={6} sx={{ paddingLeft: { xs: 0, md: 2 } }}>
+        <TableContainer component={Paper} className="overflow-x-auto">
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell className="font-bold">User ID</TableCell>
+                <TableCell align="right" className="font-bold">ID</TableCell>
+                <TableCell align="right" className="font-bold">Title</TableCell>
+                <TableCell align="right" className="font-bold">Completed</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.id} className="hover:bg-gray-100">
+                  <TableCell component="th" scope="row">
+                    {row.userId}
+                  </TableCell>
+                  <TableCell align="right">{row.id}</TableCell>
+                  <TableCell align="right">{row.title}</TableCell>
+                  <TableCell align="right">{row.completed ? 'Yes' : 'No'}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
   );
 }
